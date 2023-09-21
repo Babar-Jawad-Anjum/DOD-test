7d5f1750baca0374a1f1e98089ecb8e1f091b310
Hussnain Javed <hussnain@bracketsltd.email>
2023-09-21 20:16:56 +0500

    testing theme workflow:

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e1edbad4d8b65fe4")
+/******/ 		__webpack_require__.h = () => ("e06c8b21de422f64")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-center items-center">
-      <div className="flex justify-center items-center bg-white mt-3 h-[500px] w-1/2">
+      <div className="flex justify-center items-center bg-white mt-3 h-[500px] w-1/2 rounded-lg">
         {children}
       </div>
     </div>
